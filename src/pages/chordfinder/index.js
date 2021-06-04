058c49b8174c32e1605bc933c383abe4c89e932d
Pedro <costabreu.pedro@gmail.com>
2021-06-04 14:36:40 -0300

    teste

--- src/pages/chordfinder/index.js
+++ src/pages/chordfinder/index.js
@@ -4,9 +4,44 @@ import Logo from '../../assets/images/ScaleHunter(3).svg';
 import SideMenu from '../../components/SideMenu';
 import Data from '../../assets/data.json';
 import '../../global.css';
-import { customStyles } from '../../components/SelectStyle';
 import Footer from '../../components/Footer';
 
+const customStyles = {
+    option: (provided, state) => ({
+        ...provided,
+        color: state.isFocused ? '#FD7014' : '#eeeeee',
+        backgroundColor: state.isFocused ? '#222831' : '#393e46' ,
+        fontWeight: 'regular',
+        borderRadius: 6
+    }),
+    menu: (provided, state) => ({
+        ...provided,
+        borderRadius: 6,
+        backgroundColor: '#393e46',
+        padding: 10,
+        zIndex: 100
+    }),
+    input: (provided, state) => ({
+        ...provided,
+        color: '#eeeeee'
+    }),
+    control: () => ({
+        display: 'flex',
+        padding: 5
+    }),
+    container: (provided, state) => ({
+            ...provided,
+            backgroundColor: '#393e46',
+            borderRadius: 6,
+            marginTop: 10
+    }),
+    singleValue: (provided, state) => {
+        const opacity = 1;
+        const transition = 'opacity 300ms';
+  
+        return { ...provided, opacity, transition, color: 'white', fontSize: '18px' };
+    }
+}
 export default class ChordFinder extends React.Component{
 
     constructor(props){
